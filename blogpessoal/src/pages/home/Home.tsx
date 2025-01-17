045d7eae493cca44584262c2781cb7447f38eaf3
Michele Bueno <contato.michelebueno@gmail.com>
2025-01-16 23:50:49 -0300

    Atividade 3 prática – Tailwind css
    Projeto utilizando Tailwind css

--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -1,44 +1,27 @@
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                height: "100vh",
-                display: "flex",
-                justifyContent: "center",
-                backgroundColor: "##eccaca",
-                backgroundImage: "url('https://i.imgur.com/k7PsZdp.jpg')", 
-                backgroundSize: "cover", 
-                backgroundRepeat: "no-repeat", 
-                backgroundPosition: "center",
-                backgroundAttachment: "fixed",
-            }}>
+            <div className="w-screen flex justify-center text-green-500 font-sans">
                 <div>
-                    <div style={{
-                        width: "80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-
-                    }}>
-                        <h2>Blog da Mich </h2>
-                        <p>Meu nome é Michele e este é meu blog pessoal ฅ•ω•ฅ </p>
+                    <div className="p-6 bg-black shadow-lg">
+                        <h2 className="font-bold font-serif text-pink-400 text-4xl">Blog da Mich</h2>
+                        <br />
+                        <p> Conte-me um fato curioso </p>
+                        <p>ฅ•ω•ฅ</p>
+                        <br />
+                        <button className=" px-7 py-0 -m-2 bg-purple-700 text-white rounded-lg shadow-lg hover:bg-purple-700 hover:shadow-xl transition duration-23">
+                            Faça sua postagem
+                        </button>
                     </div>
 
-                    <div style={{
-                        width: "80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center"
-                    }}>
+                    <div className="max-w-7xl flex flex-col items-center">
                         <img
-                            src="https://scontent.fcgh8-1.fna.fbcdn.net/v/t39.30808-6/463178058_3756067721313224_6550487824077408896_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeEejIAd6LhcR47S_xDVGd9eJCOHgyu7K5YkI4eDK7srlqpai0IjuFshHug0eaJoxla5ddJY9Cqk91hzOPKZIRQw&_nc_ohc=iM27yYKjKPIQ7kNvgHZP4N9&_nc_oc=AdjD3RbAQsbnIKqEfltMKY2Tfavfq8vzG6XR5WUyfVs_q2muCy1dE0dXKffXPvuPlzN9d3ARmNiMgrLmR8ICcmmF&_nc_zt=23&_nc_ht=scontent.fcgh8-1.fna&_nc_gid=AeUA6PTKzuD1aIgL70yGJc_&oh=00_AYDI6w5nCeWVmBT473M54er_HROQV2AdKyl6_fBUSqtrdQ&oe=678D99D7"
-                            alt="Imagem da Página Home"
-                            width="400px"
+                            src="https://i.imgur.com/1RC7VHu.jpeg"
+                            alt="Imagem da Página Home, uma flor colorida, simbolizando leveza."
+                            width="500px"
                         />
                     </div>
                 </div>
-
             </div>
         </>
     )
