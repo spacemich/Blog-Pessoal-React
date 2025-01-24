3078a24a1b8937a29046363d085aa7b5b995e346
Michele Bueno <contato.michelebueno@gmail.com>
2025-01-23 21:52:41 -0300

    Criação postagens

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,18 @@
+import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem";
+
 function Home() {
     return (
         <>
             <div
                 className="min-h-screen bg-cover bg-center"
-                style={{ backgroundImage: "url('https://i.imgur.com/k7PsZdp.jpeg')",
+                style={{
+                    backgroundImage: "url('https://i.imgur.com/k7PsZdp.jpeg')",
                     backgroundSize: "cover",
                     backgroundAttachment: "fixed",
                     minHeight: "100vh",
-                    width: "100%", 
-                 }}
+                    width: "100%",
+                }}
             >
                 <div className="h-[300px] bg-[#619464]  gap-1
                 flex justify-center">
@@ -17,34 +21,36 @@ function Home() {
                             <h2 className="text-6xl font-bold">𓍢ִ໋🌷͙֒ 𝑩𝒍𝒐𝒈 𝒅𝒂 𝑴𝒊𝒄𝒉 𓍢ִ໋🌷͙֒ </h2>
                             <br />
                             <div className="flex items-center justify-between gap-2 text-2xl">
-                                <br/>
-                                
+                                <br />
+
                                 <p>𝑺𝒆𝒔𝒔𝒂̃𝒐 𝒅𝒆 𝒑𝒐𝒔𝒕𝒂𝒈𝒆𝒎:</p>
                                 <p>Deixe aqui sua opnião! </p>
                             </div>
                             <br />
+                           
                             <div className="w- [120px] h-[200px]">
                                 <br />
                                 <div
                                     className="rounded text-white 
                                             border-pink-400 border-solid border-4 py-2 px-6 bg-pink-400 hover:bg-pink-700"
                                 >
-                                    Nova Postagem
+                                    <ModalPostagem/>
                                 </div>
                             </div>
                         </div>
 
                         <div className="flex justify-end">
-                            <img 
+                            <img
                                 src="https://i.imgur.com/bUCIOXY.png"
                                 alt="Logo Capivara tomando um chá feliz ao receber a sua postagem"
                                 className="w-[400px] h-[400px]"
-                                
+
                             />
                         </div>
                     </div>
                 </div>
             </div>
+            <ListaPostagens/>
         </>
     );
 }
